646d65e39342eb218edfeb98a87beeb071573ba3
Guennator <corentin.le-guennec@epita.fr>
2017-02-07 14:30:28 +0100

    alex je te baise

---
+++ FreeForFall/Assets/WaitToFall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitToFall : MonoBehaviour {
+    private bool fall;
+    private bool done;
+
+    // Use this for initialization
+    void Start ()
+    {
+        fall = false;
+        done = false;
+    }
+    IEnumerator Wait()
+    {
+        if (done)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            yield return new WaitForSeconds(0.4f);
+            rb.isKinematic = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            print("touched");
+            done = true;
+            StartCoroutine(Wait());          
+        }
+    }
+}
