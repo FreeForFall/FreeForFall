726cc9043e691bf0f199d94c1a90937a0df4a1dd
SalmonA <alex.salmon@epita.fr>
2017-02-07 22:58:29 +0100

    Main menu

---
+++ FreeForFall/Assets/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject mainMenuHolder;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("BaseScence");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+
+    }
+}
