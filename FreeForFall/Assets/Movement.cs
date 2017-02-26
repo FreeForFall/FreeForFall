a1ea7492ffbe11b9addd59fa1e492accf91c05ec
D1ADEV <nicolasmanichon@yahoo.fr>
2017-02-26 17:48:41 +0100

    Basic networking working

---
+++ FreeForFall/Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        transform.Translate(x, 0, z);
+	}
+}
