e6af3ceb860a17dea48f72dc17f6d278676024f1
Guennator <corentin.le-guennec@epita.fr>
2017-03-08 00:30:23 +0100

    working on how to improve the map destruction

--- FreeForFall/Assets/Scripts/WaitToFall.cs
+++ FreeForFall/Assets/Scripts/WaitToFall.cs
@@ -5,12 +5,25 @@ public class WaitToFall : MonoBehaviour {
 	private float _dropDelay;
 	private float _destroyDelay;
     private float _dropforce;
+    private float _x;
+    private float _y;
+    private float _z;
+    private float _rx;
+    private float _ry;
+    private float _rz;
     private GameObject cell;
 
 	void Start() {
 		_dropDelay = 0.2f;
 		_destroyDelay = 20000000000000;
         _dropforce = 300f;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        _x = rb.position.x;
+        _y = rb.position.y;
+        _z = rb.position.z;
+        _rx = rb.rotation.x;
+        _ry = rb.rotation.y;
+        _rz = rb.rotation.z;
     }
 
 	void Drop()
@@ -26,8 +39,14 @@ public class WaitToFall : MonoBehaviour {
         Invoke("Drop", _dropDelay);
     }
 
-	void Remove()
+	void Replace()
     {
-        Destroy(gameObject);
-	}
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.position.x = _x;
+        _y = rb.position.y;
+        _z = rb.position.z;
+        _rx = rb.rotation.x;
+        _ry = rb.rotation.y;
+        _rz = rb.rotation.z;
+    }
 }
