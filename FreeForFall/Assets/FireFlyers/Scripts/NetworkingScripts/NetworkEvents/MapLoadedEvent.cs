a206d3c1b8248c753b54a219a0b016abb964ed1f
D1ADEV <nicolasmanichon@yahoo.fr>
2017-03-30 19:46:29 +0200

    added a box

---
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkEvents/MapLoadedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class MapLoadedEvent : NetworkEvent
+	{
+		public MapLoadedEvent () : base (0x1)
+		{
+		}
+	}
+}
+
---
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkEvents/RemoveWallsEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class RemoveWallsEvent : NetworkEvent
+	{
+		public RemoveWallsEvent () : base (0x3)
+		{
+		}
+	}
+}
+
