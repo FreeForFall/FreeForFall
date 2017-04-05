b8d53fe09f8f27783785e11162837c63c08540e0
Guennator <corentin.leguennec@gmail.com>
2017-04-05 21:55:24 +0200

    try to merge that shit, camera suposed to work

--- FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/PlayerController.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/PlayerController.cs
@@ -1,51 +1,75 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float maxSprintSpeed;
-	private float sprintMultiplier;
+    private float sprintMultiplier;
+    public float jumpforce;
 	private bool airbone;
 	private Rigidbody rigidBody;
+    private Quaternion rotation;
+    public float rotateSpeed = 1f;
+    private Rigidbody rb;
+    public Transform playerCamera;
 
-	// Use this for initialization
-	void Start ()
-	{
-		this.sprintMultiplier = 1f;
+    // Use this for initialization
+    void Start () {
+	    this.sprintMultiplier = 1f;
 		this.airbone = false;
-		this.rigidBody = this.GetComponent<Rigidbody> ();
+		this.rigidBody = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        
+
 	}
 
-	void OnTriggerEnter (Collider other)
-	{
-		this.airbone = false;
-	}
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "ground")
+        {
+            this.airbone = false;
+        }
+    }
 
-	private void doMovement ()
-	{
-		if ((Input.GetKeyDown (KeyCode.Joystick1Button0) || Input.GetKeyDown (KeyCode.Space)) && !this.airbone)
-		{
-			this.airbone = true;
-			this.rigidBody.AddForce (new Vector3 (0f, 50000f, 0f));
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.name == "ground")
+        {
+            this.airbone = true;
+        }
+    }
+
+
+    private void doMovement(){
+		if((Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Space)) && !this.airbone){
+			this.rigidBody.AddForce(new Vector3(0f, jumpforce , 0f));
 		}
-		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.Joystick1Button8))
-		{
+        if((Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.F)))
+        {
+            BroadcastMessage("IsFallen",100f);
+        }
+		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.Joystick1Button8)){
 			this.sprintMultiplier = this.maxSprintSpeed;
-		}
-		else
-		{
+		} else {
 			this.sprintMultiplier = 1f;
 		}
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
-		transform.Translate (Vector3.right * h * Time.deltaTime * this.speed * this.sprintMultiplier);
-		transform.Translate (Vector3.forward * v * Time.deltaTime * this.speed * this.sprintMultiplier);
-	}
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h> 0 || h < 0 || v > 0 || v < 0)
+        {
+            float rotate = rotateSpeed * Time.deltaTime;
+            Vector3 direction = new Vector3(h, 0, v);
+            rotation = Quaternion.LookRotation(direction, Vector3.up);
+            Quaternion cam = new Quaternion(0, playerCamera.rotation.y, 0, playerCamera.rotation.w);
+            rb.rotation = Quaternion.RotateTowards(transform.rotation, cam * rotation, rotate);
+            Vector3 Moveto = new Vector3(0, transform.position.y,1);
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward * this.speed * Time.deltaTime, 1f);     
+        }
+    }
 
 	// Update is called once per frame
 	void Update ()
-	{
-		doMovement ();
-	}
+    {    
+        doMovement();
+    }
 }
