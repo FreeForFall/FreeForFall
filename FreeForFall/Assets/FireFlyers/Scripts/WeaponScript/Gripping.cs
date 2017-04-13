c3405dad2a0fc568490dbf7b86568bf36d9cadf0
Stan Sokolov <stanislas.sokolov@epita.fr>
2017-04-13 17:33:13 +0200

    Updated Grip, you need to test this, change some values and i need some clarification about the maps. Right clic = graplin, Left clic = explosives. I need to create a weapon range scene to test but a player spawned in a new scene is bugged.

---
+++ FreeForFall/Assets/FireFlyers/Scripts/WeaponScript/Gripping.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gripping : MonoBehaviour {
+
+    public float PullForce;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        this.transform.parent.transform.GetComponent<Rigidbody>().AddForce((this.transform.parent.transform.position - transform.position).normalized * -PullForce);
+        Destroy(this.gameObject);
+    }
+ }
