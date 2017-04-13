e913e5eb660477153dd9853427721fbbaa1d4718
Guennator <corentin.leguennec@gmail.com>
2017-04-13 15:58:51 +0200

    TPS done and polished / FPS kinda working fine but some wierd glith happen ... wierd direction changes, idk it might be a sensivity problem ...

--- FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/CameraControl.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/CameraControl.cs
@@ -16,6 +16,7 @@ public class CameraControl : MonoBehaviour {
     public Camera Principale;
     public Camera Secondaire;
     public Transform FPSAnchor;
+    public Transform FPSlookto;
     private bool istps = true;
     private Vector3 eulerAngle;
     private float verticalOrientation;
@@ -31,7 +32,7 @@ public class CameraControl : MonoBehaviour {
         Secondaire.enabled = false;
         Principale.enabled = true;
         Cursor.visible = false;
-        initial_distance = new Vector3(1,2,-15);
+        initial_distance = new Vector3(2,2,-15);
         if (target != null)
         {
             transform.position = player.position + initial_distance;
@@ -43,7 +44,13 @@ public class CameraControl : MonoBehaviour {
     {
         float h = Input.GetAxis("Mouse X");
         float v = Input.GetAxis("Mouse Y");
-        Principale.transform.position = Vector3.MoveTowards(Principale.transform.position, transform.position, 40*0.125f);
+        if (istps)
+        {
+            Principale.transform.position = Vector3.MoveTowards(Principale.transform.position, transform.position, 60 * 0.125f);
+            Principale.transform.rotation = transform.rotation;
+        }
+        else
+            Principale.transform.position = Vector3.MoveTowards(Principale.transform.position, FPSAnchor.position, 60 * 0.125f);
         if (target != null)
         {
             
@@ -56,25 +63,33 @@ public class CameraControl : MonoBehaviour {
             }
             relativeDistance = transform.position - target.position;
         }
-        if (v != 0)
+        if (v != 0 && istps)
         {
             if (transform.position.y < target.position.y + MaxCameraAngleUp && v > 0)
             {
                 Vector3 mouvement_up = new Vector3(0, v + sensivity , 0);
-                relativeDistance = Vector3.Slerp(relativeDistance, relativeDistance + mouvement_up,2* 0.125f);
+                relativeDistance = Vector3.Slerp(relativeDistance, relativeDistance + mouvement_up, 2 * 0.125f);
             }
             if(transform.position.y > target.position.y - MaxCameraAngleDown && v <0)
             {
                 Vector3 mouvement_up = new Vector3(0, v - sensivity , 0);
-                relativeDistance = Vector3.Slerp(relativeDistance, relativeDistance + mouvement_up,2 * 0.125f);
+                relativeDistance = Vector3.Slerp(relativeDistance, relativeDistance + mouvement_up, 2 * 0.125f);
 
             }
+        }
+        if (v !=0 && !istps)
+        {
+            horizontalOrientation = h * 5;
+            verticalOrientation = v * 5;
+            this.eulerAngle.x -= verticalOrientation;
+            this.eulerAngle.y += horizontalOrientation;
+            Principale.transform.eulerAngles = this.eulerAngle;
         }
     }
 
     void FPSview()
     {
-        Principale.transform.position = Vector3.MoveTowards(Principale.transform.position, FPSAnchor.position, 80 * 0.125f);
+        Principale.transform.position = Vector3.MoveTowards(Principale.transform.position, FPSAnchor.position, 40 * 0.125f);
         horizontalOrientation = Input.GetAxis("Mouse X") * 5;
         verticalOrientation = Input.GetAxis("Mouse Y") * 5;
         this.eulerAngle.x -= verticalOrientation;
@@ -88,15 +103,31 @@ public class CameraControl : MonoBehaviour {
         {
             istps = !istps;
         }
+        OrbitTPS();
+        /*
         if (istps)
         {
-
             OrbitTPS();
         }
         else
         {
             FPSview();   
         }
+        */
+    }
+
+    void LateUpdate()
+    {
+        if (istps)
+        {
+            lookto();
+        }
+    }
+
+    void lookto()
+    {
+        Vector3 lookto = new Vector3(target.position.x, target.position.y + 2, target.position.z);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookto - transform.position), 40 * 0.125f);
     }
 }
 
