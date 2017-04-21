3a55a33e70d27f68b8bee403aa03fd1cafdabf32
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-21 22:37:18 +0200

    cleaned up

---
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/WheelAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelAnimation : MonoBehaviour
+{
+
+	public bool left = false;
+	public float sensivity;
+	// Use this for initialization
+	void Start ()
+	{
+		sensivity = -200;
+	}
+	
+	// Update is called once per frame
+
+	void Update ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		if (h != 0 || v != 0)
+		{
+			transform.Rotate (Vector3.left * Time.deltaTime * sensivity, Space.Self);
+		}
+	}
+}
