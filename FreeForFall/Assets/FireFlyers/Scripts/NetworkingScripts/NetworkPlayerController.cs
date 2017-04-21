e3daa2497af72116b101edc96723643340c7cb32
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-21 12:35:18 +0200

    maybe fixed

--- FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
@@ -8,12 +8,15 @@ public class NetworkPlayerController : MonoBehaviour
 	private Quaternion _targetRotation;
 
 	private PhotonView _photonView;
+
+	private Transform _bottom;
 	// Use this for initialization
 	void Start ()
 	{
 		_targetPosition = transform.position;
 		_targetRotation = Quaternion.Euler (0, 0, 0);
 		_photonView = GetComponent<PhotonView> ();
+		_bottom = transform.Find ("Bottom").transform;
 	}
 	
 	// Update is called once per frame
@@ -21,8 +24,8 @@ public class NetworkPlayerController : MonoBehaviour
 	{
 		if (!_photonView.isMine)
 		{
-			transform.position = Vector3.Lerp (transform.position, _targetPosition, 0.1f); // Maybe add velocity scaling
-			transform.rotation = _targetRotation;
+			_bottom.position = Vector3.Lerp (_bottom.position, _targetPosition, 0.1f); // Maybe add velocity scaling
+			_bottom.rotation = _targetRotation;
 		}
 	}
 
@@ -30,10 +33,11 @@ public class NetworkPlayerController : MonoBehaviour
 	{
 		if (pStream.isWriting)
 		{
-			pStream.SendNext (transform.position);
-			pStream.SendNext (transform.rotation);
+			pStream.SendNext (_bottom.position);
+			pStream.SendNext (_bottom.rotation);
 		}
-		else if (pStream.isReading)
+		else
+		if (pStream.isReading)
 		{
 			_targetPosition = (Vector3)pStream.ReceiveNext ();
 			_targetRotation = (Quaternion)pStream.ReceiveNext ();
