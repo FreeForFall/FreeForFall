733b626a6fd448a68e7cd07085aa7184e71aa83d
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-23 16:13:39 +0200

    Modified the map, there's still an issue with the floor

--- FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/WaitToFall.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/WaitToFall.cs
@@ -1,55 +1,57 @@
 ﻿using UnityEngine;
 
-public class WaitToFall : MonoBehaviour {
+public class WaitToFall : MonoBehaviour
+{
 
 	private float _dropDelay;
 	private float _destroyDelay;
-    private float _dropforce;
-    public bool _isFallen = false;
-    private GameObject cell;
-    public float speed = 5;
-    public float rotateSpeed = 360;
+	private float _dropforce;
+	public bool _isFallen = false;
+	private GameObject cell;
+	public float speed = 5;
+	public float rotateSpeed = 360;
 
-    void Start() {
+	void Start ()
+	{
 		_dropDelay = 0.2f;
 		_destroyDelay = 20000000000000;
-        _dropforce = 300f;
-    }
-
-
-    void Goback()
-    {
-        Renderer Ren = GetComponent<Renderer>();
-        MeshCollider Mcol = GetComponent<MeshCollider>();
-        Mcol.enabled = true;
-        Ren.enabled = true;
-        GetComponent<ReturnToPosition>().enabled = true;
-    }
-
-    void IsFallen()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        Renderer Ren = GetComponent<Renderer>();
-        MeshCollider Mcol = GetComponent<MeshCollider>();
-        Mcol.enabled = false;
-        Ren.enabled = false;
-        rb.isKinematic = true;
-        _isFallen = true;
-    }
-
-    void Drop()
-    {
-        if (!_isFallen)
-        {
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.isKinematic = false;
-            rb.AddForce(new Vector3(0f, -_dropforce, 0f));
-            _isFallen = true;
-        }
+		_dropforce = 600f;
 	}
 
-    void CallDrop()
-    {
-        Invoke("Drop", _dropDelay);
-    }
+
+	void Goback ()
+	{
+		Renderer Ren = GetComponent<Renderer> ();
+		MeshCollider Mcol = GetComponent<MeshCollider> ();
+		Mcol.enabled = true;
+		Ren.enabled = true;
+		GetComponent<ReturnToPosition> ().enabled = true;
+	}
+
+	void IsFallen ()
+	{
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		Renderer Ren = GetComponent<Renderer> ();
+		MeshCollider Mcol = GetComponent<MeshCollider> ();
+		Mcol.enabled = false;
+		Ren.enabled = false;
+		rb.isKinematic = true;
+		_isFallen = true;
+	}
+
+	void Drop ()
+	{
+		if (!_isFallen)
+		{
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			rb.isKinematic = false;
+			rb.AddForce (new Vector3 (0f, -_dropforce, 0f));
+			_isFallen = true;
+		}
+	}
+
+	void CallDrop ()
+	{
+		Invoke ("Drop", _dropDelay);
+	}
 }
