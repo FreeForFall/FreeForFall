6af47f93ddc9e40952370827d98c5f6b577381fa
Nicolas M <d1adev@users.noreply.github.com>
2017-04-24 18:27:59 +0200

    Game ending (#65)
    Added nickname support and the end of the game is dope af dude

--- FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/PlayerCollector.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/PlayerCollector.cs
@@ -5,6 +5,15 @@ using AssemblyCSharp;
 
 public class PlayerCollector : MonoBehaviour
 {
+	private GameObject _localPlayer;
+	private NetworkScript _networking;
+
+	void Start ()
+	{
+		_localPlayer = GameObject.Find ("NetworkManager").GetComponent<NetworkScript> ().Player;
+		_networking = GameObject.Find ("NetworkManager").GetComponent<NetworkScript> ();
+	}
+
 	void OnCollisionEnter (Collision col)
 	{
 		if (col.gameObject.tag != "Player")
@@ -12,8 +21,17 @@ public class PlayerCollector : MonoBehaviour
 			Debug.Log ("Collided with something that wasn't a player");
 			return;
 		}
-		Debug.Log ("Collided with a player");
-		NetworkEventHandlers.SendEvent (new PlayerLostEvent ());
-		GameObject.Find ("NetworkManager").GetComponent<NetworkScript> ().handlePlayerLost ();
+		Debug.LogWarning ("Collided with a player");
+		GameObject p = col.gameObject.name != "Player" 
+			? col.gameObject.transform.parent.gameObject 
+			: col.gameObject;// I don't think this is ever going to happen but we never know
+		if (p == _localPlayer)
+		{
+			Debug.LogWarning ("The local player collided");
+			_networking.SwitchToSpecView ();
+		}
+		if (PhotonNetwork.isMasterClient)
+			_networking.handlePlayerLost ();
+		Destroy (p);
 	}
 }
--- FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkScript.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkScript.cs
@@ -29,12 +29,27 @@ namespace AssemblyCSharp
 		private Button _roomCreationButton;
 		private Button _refreshButton;
 		private InputField _nameInput;
+		private InputField _nicknameInput;
 		private Text _roomList;
 		private Text _playerCountText;
 		private Button _startGameButton;
 
 		private GameObject _player;
 		private GameObject _camera;
+
+		private Vector3 _cameraStartPosition;
+
+		private string _nickname;
+
+		public GameObject Player
+		{
+			get
+			{
+				return _player;
+			}
+		}
+
+		public Camera FlyingCamera;
 
 		private int _loadedCount;
 		private int _lostCount;
@@ -55,6 +70,7 @@ namespace AssemblyCSharp
 			_matchMakingCanvas = Instantiate (_matchMakingCanvas);
 			init ();
 			connectToServer (GameObject.Find ("SettingsManager").GetComponent<Settings> ().OnlineMode);
+			_cameraStartPosition = FlyingCamera.transform.position;
 		}
 
 		// Need to introduce safe casting of the objects sent over the network, this is really dangerous.
@@ -70,7 +86,6 @@ namespace AssemblyCSharp
 					break;
 				case 0x3:
 				// remove walls
-					Invoke ("switchCamera", 3);
 					Invoke ("destroyBox", 5);
 					break;
 				case 0x19:
@@ -89,6 +104,13 @@ namespace AssemblyCSharp
 
 		private void endGame ()
 		{
+			/*
+
+			Replace this method by something that shows a prefab
+			When it does, remove the Invoke("endGame") from handlePlayerLost, 
+			because it won't be needed anymore
+
+			*/
 			PhotonNetwork.Disconnect ();
 			SceneManager.LoadScene ("Menu");
 		}
@@ -111,7 +133,7 @@ namespace AssemblyCSharp
 			}
 			Debug.Log ("A player lost and was the last man standing.");
 			NetworkEventHandlers.SendEvent (new EndGameEvent ());
-			endGame ();
+			Invoke ("endGame", 6);
 		}
 
 		private void removeWalls ()
@@ -122,15 +144,45 @@ namespace AssemblyCSharp
 			{
 				Debug.Log ("REMOVING WALLS");
 				NetworkEventHandlers.SendEvent (new RemoveWallsEvent ());
-				Invoke ("switchCamera", 3);
+				//Invoke ("switchCamera", 3);
 				Invoke ("destroyBox", 5);
 			}
 		}
 
+		public void SwitchToSpecView ()
+		{
+			switchCamera ();
+			StartCoroutine (MoveObject (FlyingCamera.transform, _camera.transform.position, _cameraStartPosition, 5f));
+		}
+
+		IEnumerator MoveObject (Transform obj, Vector3 source, Vector3 target, float overTime)
+		{
+			float startTime = Time.time;
+			while (Time.time < startTime + overTime)
+			{
+				obj.position = Vector3.Lerp (source, target, (Time.time - startTime) / overTime);
+				yield return null;
+			}
+			obj.position = target;
+		}
+
+
 		private void switchCamera ()
 		{
-			GameObject.Find ("Camera").SetActive (false);
-			_camera.GetComponent<Camera> ().enabled = true;
+			Debug.LogWarning ("SWITCHING CAMERAS");
+			if (FlyingCamera.gameObject.GetActive ())
+			{
+				FlyingCamera.gameObject.GetComponent<Camera> ().enabled = false;
+				FlyingCamera.gameObject.SetActive (false);
+				_camera.SetActive (true);
+			}
+			else
+			{
+				FlyingCamera.gameObject.GetComponent<Camera> ().enabled = true;
+				FlyingCamera.gameObject.SetActive (true);
+				_camera.SetActive (false); 
+			}
+
 		}
 
 		private void destroyBox ()
@@ -159,6 +211,7 @@ namespace AssemblyCSharp
 
 		private void loadMap (string name)
 		{
+			FlyingCamera.gameObject.SetActive (false);
 			Debug.Log ("Loading map " + name);
 			_map = (GameObject)Instantiate (Resources.Load (name), Vector3.zero, Quaternion.identity);
 			Destroy (_waitForGameStartCanvas);
@@ -168,16 +221,17 @@ namespace AssemblyCSharp
 			_player = PhotonNetwork.Instantiate ("Player", spawnPosition, Quaternion.identity, 0);
 			_player.GetComponent<CrosshairUI> ().enabled = true;
 			_player.GetComponentInChildren<PlayerController> ().enabled = true;
-            _player.GetComponent<ShooterB>().enabled = true;
-			//_player.GetComponentInChildren<LookTowardCamera> ().enabled = true;
+			_player.GetComponent<ShooterB> ().enabled = true;
 			_player.GetComponentInChildren<CameraControl> ().enabled = true;
-			_player.transform.Find ("TPScamera/firstCamera").gameObject.GetComponent<Camera> ().enabled = true;
-			// Don't forget to remove the camera of the other players
-			removeWalls ();
+			_camera = _player.transform.Find ("TPScamera/firstCamera").gameObject;
+
+			// SET THE NICKNAME CANVAS
+
 			if (!PhotonNetwork.isMasterClient)
 			{
 				NetworkEventHandlers.SendEvent (new MapLoadedEvent ());
 			}
+			removeWalls ();
 		}
 
 		private void init ()
@@ -213,7 +267,7 @@ namespace AssemblyCSharp
 		void OnGUI ()
 		{
 			if (PhotonNetwork.isMasterClient)
-				GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString () + " Count in room : " + PhotonNetwork.room.PlayerCount + " Count Loaded : " + _loadedCount);
+				GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString () + " Count in room : " + PhotonNetwork.room.PlayerCount + " Count Loaded : " + _loadedCount + " Count Lost : " + _lostCount);
 			else
 				GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
 		}
@@ -224,11 +278,13 @@ namespace AssemblyCSharp
 			_refreshButton = GameObject.Find ("RefreshButton").GetComponent<Button> ();
 			_nameInput = GameObject.Find ("RoomNameInput").GetComponent<InputField> ();
 			_roomList = GameObject.Find ("RoomListView").GetComponent<Text> ();
+			_nicknameInput = GameObject.Find ("NicknameInput").GetComponent<InputField> ();
 		}
 
 		private void roomCreationClick ()
 		{
-			Debug.Log ("Creating a room with text : " + _nameInput.text);
+			Debug.Log ("Creating a room with text : " + _nameInput.text + " by nickname : " + _nicknameInput.text);
+			_nickname = _nicknameInput.text;
 			var options = new RoomOptions ();
 			options.MaxPlayers = 255;
 			PhotonNetwork.JoinOrCreateRoom (_nameInput.text, options, null);
