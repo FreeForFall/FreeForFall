759bc116dd825fc63ae967f3b977e5bbb2060125
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-25 02:13:44 +0200

    Syncing the rotation

--- FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
@@ -4,22 +4,25 @@ public class NetworkPlayerController : MonoBehaviour
 {
 	private Vector3 _targetPosition;
 	// Need to find a way to smooth the rotation
-	private Quaternion _targetRotation;
+	private Quaternion _targetBottomRotation;
+
+	private Quaternion _targetTorsoRotation;
 
 	private PhotonView _photonView;
 
 	private Transform _bottom;
+
+	private Transform _torso;
 	// Use this for initialization
 	void Start ()
 	{
 		Debug.LogError (gameObject);
 		
 		_photonView = GetComponent<PhotonView> ();
-		Debug.LogError ("photon view");
-		Debug.Log (_photonView.isMine);
 		_bottom = gameObject.transform.Find ("bottom");
+		_torso = gameObject.transform.Find ("Torso");
 		_targetPosition = _bottom.position;
-		_targetRotation = Quaternion.Euler (0, 0, 0);
+		_targetBottomRotation = Quaternion.Euler (0, 0, 0);
 	}
 	
 	// Update is called once per frame
@@ -28,7 +31,8 @@ public class NetworkPlayerController : MonoBehaviour
 		if (!_photonView.isMine)
 		{
 			_bottom.position = Vector3.Lerp (_bottom.position, _targetPosition, 0.1f); // Maybe add velocity scaling
-			_bottom.rotation = _targetRotation;
+			_bottom.rotation = Quaternion.Lerp (_bottom.rotation, _targetBottomRotation, 0.1f);
+			_torso.rotation = Quaternion.Lerp (_torso.rotation, _targetTorsoRotation, 0.1f);
 		}
 	}
 
@@ -38,12 +42,14 @@ public class NetworkPlayerController : MonoBehaviour
 		{
 			pStream.SendNext (_bottom.position);
 			pStream.SendNext (_bottom.rotation);
+			pStream.SendNext (_torso.rotation);
 		}
 		else
 		if (pStream.isReading)
 		{
 			_targetPosition = (Vector3)pStream.ReceiveNext ();
-			_targetRotation = (Quaternion)pStream.ReceiveNext ();
+			_targetBottomRotation = (Quaternion)pStream.ReceiveNext ();
+			_targetTorsoRotation = (Quaternion)pStream.ReceiveNext ();
 		}
 	}
 }
