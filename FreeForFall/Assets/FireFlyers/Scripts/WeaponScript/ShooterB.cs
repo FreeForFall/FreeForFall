fb217546aab89babc4ca481322bf3f62c72e3ba5
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-26 00:55:20 +0200

    networking

--- FreeForFall/Assets/FireFlyers/Scripts/WeaponScript/ShooterB.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/WeaponScript/ShooterB.cs
@@ -39,12 +39,15 @@ public class ShooterB : MonoBehaviour
 			if (Input.GetMouseButtonDown (0) || Input.GetKey (KeyCode.Joystick1Button5))
 			{
 				TimeSinceLastExplosion = 0;
-				GameObject temp_projectile;
-				temp_projectile = Instantiate (projectile, Launcher.transform.position, Launcher.transform.rotation) as GameObject;
-				Rigidbody projectile_body;
-				projectile_body = temp_projectile.GetComponent<Rigidbody> ();
-				projectile_body.AddForce (Camera.transform.forward * projectile_force);
-				Destroy (temp_projectile, 10.0f);
+//				GameObject temp_projectile;
+//				temp_projectile = Instantiate (projectile, Launcher.transform.position, Launcher.transform.rotation) as GameObject;
+//				Rigidbody projectile_body;
+//				projectile_body = temp_projectile.GetComponent<Rigidbody> ();
+//				projectile_body.AddForce (Camera.transform.forward * projectile_force);
+//				Destroy (temp_projectile, 10.0f);
+				TimeSinceLastGrip = 0;
+				NetworkEventHandlers.SendEvent (new BazookaEvent (Launcher.transform.position, Launcher.transform.rotation, Camera.transform.forward * projectile_force));
+				_network.HandleBazooka (Launcher.transform.position, Launcher.transform.rotation, Camera.transform.forward * projectile_force);
 			}
 		}
 
