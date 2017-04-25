3209c9d9edb2423da249658f6c5e752a58683331
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-04-25 23:24:44 +0200

    Revert "Trying something else"
    This reverts commit 58f47801a7407576dfaa785fd8599654c77c45ec.

--- FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/NetworkingScripts/NetworkPlayerController.cs
@@ -13,13 +13,11 @@ public class NetworkPlayerController : MonoBehaviour
 	private Transform _bottom;
 
 	private Transform _torso;
-
-	private float _timeSinceLastSerialize;
 	// Use this for initialization
 	void Start ()
 	{
 		Debug.LogError (gameObject);
-		_timeSinceLastSerialize = 0.05f;
+		
 		_photonView = GetComponent<PhotonView> ();
 		_bottom = gameObject.transform.Find ("bottom");
 		_torso = gameObject.transform.Find ("Torso");
@@ -32,12 +30,11 @@ public class NetworkPlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		_timeSinceLastSerialize += Time.deltaTime;
 		if (!_photonView.isMine)
 		{
-			_bottom.position = Vector3.Lerp (_bottom.position, _targetPosition, _timeSinceLastSerialize); // Maybe add velocity scaling
-			_bottom.rotation = Quaternion.Lerp (_bottom.rotation, _targetBottomRotation, _timeSinceLastSerialize);
-			_torso.rotation = Quaternion.Lerp (_torso.rotation, _targetTorsoRotation, _timeSinceLastSerialize);
+			_bottom.position = Vector3.Lerp (_bottom.position, _targetPosition, 0.1f); // Maybe add velocity scaling
+			_bottom.rotation = Quaternion.Lerp (_bottom.rotation, _targetBottomRotation, 0.1f);
+			_torso.rotation = Quaternion.Lerp (_torso.rotation, _targetTorsoRotation, 0.1f);
 		}
 	}
 
@@ -52,7 +49,6 @@ public class NetworkPlayerController : MonoBehaviour
 		else
 		if (pStream.isReading)
 		{
-			_timeSinceLastSerialize = 0f;
 			_bottom.position = _targetPosition;
 			_bottom.rotation = _targetBottomRotation;
 			_torso.rotation = _targetTorsoRotation;
