f92be688dedca2b029029dbf4f514d5427819afc
Stan Sokolov <stanislas.sokolov@epita.fr>
2017-05-13 00:12:52 +0200

    AI incomming

--- FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/PlayerController.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour
 
 	void OnCollisionEnter (Collision other)
 	{
-		if (other.gameObject.tag == "Ground")
+		if (other.gameObject.name == "ground")
 		{
 			this.airbone = false;
 		}
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour
 
 	void OnCollisionExit (Collision other)
 	{
-		if (other.gameObject.tag == "Ground")
+		if (other.gameObject.name == "ground")
 		{
 			this.airbone = true;
 		}
