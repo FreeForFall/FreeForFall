854ba9e04fb0914bd21c5fe35e39a55d01c9e417
Stan Sokolov <stanislas.sokolov@epita.fr>
2017-05-13 19:18:16 +0200

    The AI can jump, it fuckin amazing

--- FreeForFall/Assets/FireFlyers/Scripts/AIScripts/SimpleAI.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/AIScripts/SimpleAI.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class SimpleAI : MonoBehaviour {
 
+    public AudioSource jumpsound;
+    public GameObject jumpeffect;
+    public Transform jumpflamelocation;
     public float sensorLength = 2f;
     public float speed = 30f;
     public float turnspeed = 1000f;
     private int turnValue = 0;
     private System.Random rng;
     public const float JUMP_FORCE = 150000f; // to remove
+    public const float JUMP_CD = 2f; // to remove
+    private float Timesincelastjump;
     private bool airbone;
     private bool jumping;
 
     // Use this for initialization
     void Start () {
+        Timesincelastjump = 3;
         this.airbone = false;
         this.jumping = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Timesincelastjump += Time.deltaTime;
         air();
         if(!airbone)
         {
             Mouvement();
-            if (Input.GetKeyDown(KeyCode.Space))
-                jump();
         }
         if (jumping)
             transform.position += transform.forward * speed * Time.deltaTime;
@@ -35,36 +40,46 @@ public class SimpleAI : MonoBehaviour {
 
     void OnDrawGizmos ()
     {
-        Gizmos.DrawRay(transform.position + 2 * transform.forward + transform.up, (transform.forward + -transform.up) *(sensorLength + transform.localScale.z));
-        Gizmos.DrawRay(transform.position + 2 * transform.forward + transform.up, (-transform.up + transform.right + transform.forward) *(sensorLength + transform.localScale.z));
-        Gizmos.DrawRay(transform.position + 2 * transform.forward + transform.up, (-transform.up + -transform.right + transform.forward) *(sensorLength + transform.localScale.z));
-        Gizmos.DrawRay(transform.position - transform.up * 1f, (-transform.up) * (1));
-        Gizmos.DrawRay(transform.position - transform.up * 1f + transform.right, (-transform.up) * (1));
-        Gizmos.DrawRay(transform.position - transform.up * 1f + -transform.right, (-transform.up) * (1));
+        Gizmos.DrawRay(transform.position + 0.5f * transform.forward + transform.up, (transform.forward + -transform.up) *(sensorLength + transform.localScale.z));
+        Gizmos.DrawRay(transform.position + 0.5f * transform.forward + transform.up, (-transform.up + transform.right ) *(sensorLength + transform.localScale.z));
+        Gizmos.DrawRay(transform.position + 0.5f * transform.forward + transform.up, (-transform.up + -transform.right ) *(sensorLength + transform.localScale.z));
+        Gizmos.DrawRay(transform.position - transform.up * 1f + -transform.right + transform.forward, (-transform.up) * (1));
+        Gizmos.DrawRay(transform.position - transform.up * 1f + transform.right + transform.forward, (-transform.up) * (1));
+        Gizmos.DrawRay(transform.position - transform.up * 1f + -transform.right + -transform.forward, (-transform.up) * (1));
+        Gizmos.DrawRay(transform.position - transform.up * 1f + transform.right + -transform.forward, (-transform.up) * (1));
+        Gizmos.DrawRay(transform.position - transform.up * 1f + transform.forward*8, (-transform.up) * (1));
     }
 
     void Mouvement()
     {
         RaycastHit hit;
         int flag = 0;
-        if (!Physics.Raycast(transform.position + 2 * transform.forward + transform.up, transform.forward + -transform.up, out hit, (sensorLength + transform.localScale.z)))
+        if (!Physics.Raycast(transform.position + 0.5f * transform.forward + transform.up, transform.forward + -transform.up, out hit, (sensorLength + transform.localScale.z)))
         {
-            if (!Physics.Raycast(transform.position + 2 * transform.forward + transform.up, transform.forward + -transform.up + transform.right, out hit, (sensorLength + transform.localScale.z)))
+            if (Physics.Raycast(transform.position + 1 * transform.forward * 8, -transform.up, out hit, 3) && JUMP_CD < Timesincelastjump) //change here
             {
-                turnValue -= 1;
-                flag++;
-            }
-            else if (!Physics.Raycast(transform.position + 2 * transform.forward + transform.up, transform.forward + -transform.up + -transform.right, out hit, (sensorLength + transform.localScale.z)))
-            {
-                turnValue += 1;
-                flag++;
+                jump();
             }
             else
             {
-                var x = Random.Range(-1, 2);
-                turnValue += x;
-                flag++;
+                if (!Physics.Raycast(transform.position + 0.5f * transform.forward + transform.up, -transform.up + transform.right, out hit, (sensorLength + transform.localScale.z)))
+                {
+                    turnValue -= 1;
+                    flag++;
+                }
+                else if (!Physics.Raycast(transform.position + 1.5f * transform.forward + transform.up, transform.forward + -transform.up + -transform.right, out hit, (sensorLength + transform.localScale.z)))
+                {
+                    turnValue += 1;
+                    flag++;
+                }
+                else
+                {
+                    var x = Random.Range(-1, 2);
+                    turnValue += x;
+                    flag++;
+                }
             }
+            
         }
         if (flag == 0)
             turnValue = 0;
@@ -75,23 +90,28 @@ public class SimpleAI : MonoBehaviour {
     private void jump()
     {
         jumping = true;
-        GetComponent<Rigidbody>().AddForce(new Vector3(0f,  JUMP_FORCE, 0f));
+        GameObject JumpeffectDone = Instantiate(jumpeffect, jumpflamelocation.transform.position, jumpflamelocation.transform.rotation) as GameObject;
+        jumpsound.Play();
+        Timesincelastjump = 0;
+        GetComponent<Rigidbody>().AddForce(new Vector3(0f,  JUMP_FORCE, 0f)); //change here
         Invoke("jumpDown", 0.3f);
+        Destroy(JumpeffectDone, 0.5f);
 
     }
 
     private void jumpDown()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(0f, -JUMP_FORCE * 1.5f, 0f));
+        GetComponent<Rigidbody>().AddForce(new Vector3(0f, -JUMP_FORCE * 1.5f, 0f)); //change here
         Invoke("canceljump", 0.3f);
     }
 
     private void air()
     {
         RaycastHit hit;
-        if (!Physics.Raycast(transform.position - transform.up * 1f, -transform.up, out hit, (0.5f))
-            && !Physics.Raycast(transform.position - transform.up * 1f + -transform.right, -transform.up, out hit, (0.5f))
-            && !Physics.Raycast(transform.position - transform.up * 1f + transform.right, -transform.up, out hit, (0.5f))) // add more raycasts
+        if (!Physics.Raycast(transform.position - transform.up * 1f + transform.right + transform.forward, -transform.up, out hit, (0.5f))
+            && !Physics.Raycast(transform.position - transform.up * 1f + -transform.right +transform.forward, -transform.up, out hit, (0.5f))
+            && !Physics.Raycast(transform.position - transform.up * 1f + transform.right - transform.forward, -transform.up, out hit, (0.5f))
+            && !Physics.Raycast(transform.position - transform.up * 1f + -transform.right - transform.forward, -transform.up, out hit, (0.5f)))
         {
             airbone = true;
         }
