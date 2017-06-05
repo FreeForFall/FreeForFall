f9c0311f4bcb935b4eb694e0370062333dba3171
Stan Sokolov <stanislas.sokolov@epita.fr>
2017-06-05 18:13:28 +0200

    forcefield working

---
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/FuckingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuckingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision CollisionInfo)
+    {
+        if (CollisionInfo.collider.tag == "forcefield")
+        {
+            this.GetComponent<Rigidbody>().AddForce(-transform.position * 50, ForceMode.Impulse);
+        }
+    }
+}
