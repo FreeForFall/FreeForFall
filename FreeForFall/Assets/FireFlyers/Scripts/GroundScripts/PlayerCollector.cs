0ae416d8fd97fd4baf40f66ed3e0becfc0d0abbd
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-06-07 01:31:05 +0200

    The AIs are removed when they hit the bottom

--- FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/PlayerCollector.cs
+++ FreeForFall/Assets/FireFlyers/Scripts/GroundScripts/PlayerCollector.cs
@@ -5,31 +5,28 @@ using AssemblyCSharp;
 
 public class PlayerCollector : MonoBehaviour
 {
-	private Networking _networking;
+    private Networking _networking;
 
-	void Start ()
-	{
-		_networking = GameObject.Find ("NetworkManager").GetComponent<Networking> ();
-	}
+    void Start()
+    {
+        _networking = GameObject.Find("NetworkManager").GetComponent<Networking>();
+    }
 
-	void OnCollisionEnter (Collision col)
-	{
-		if (col.gameObject.tag != "Player")
-		{
-			return;
-		}
-		GameObject p = col.gameObject.name != "Player" 
-			? col.gameObject.transform.parent.gameObject 
-			: col.gameObject;// I don't think this is ever going to happen but we never know
-		if (p == _networking.Player)
-		{
-			_networking.Engine.SwitchToSpecView ();
-		}
-		if (PhotonNetwork.isMasterClient)
-		{
-			_networking.Engine.PlayerLost (p);
-		}
-		// Add server stuff
-		Destroy (p);
-	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        GameObject p = col.transform.root.gameObject;
+        if (p.GetComponent<NetworkPlayerController>() != null)
+        {
+            if (p == _networking.Player)
+                _networking.Engine.SwitchToSpecView();
+            if (PhotonNetwork.isMasterClient)
+                _networking.Engine.PlayerLost(p);
+        }
+        Destroy(p);
+    }
 }
