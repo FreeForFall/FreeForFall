550c811ac7b21b53e8fefafe9f8120ca2885df6b
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-06-08 01:17:03 +0200

    Canvas looks at the camera

---
+++ FreeForFall/Assets/FireFlyers/Scripts/PlayerScripts/CanvasLookTowardCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLookTowardCamera : MonoBehaviour
+{
+
+    // Use this for initialization
+    private Transform _camera;
+    void Start()
+    {
+        _camera = GameObject.Find("NetworkManager")
+            .GetComponent<Networking>().Engine.FPSCamera.transform;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_camera.transform.position);
+        transform.Rotate(0, 180, 0);
+    }
+}
